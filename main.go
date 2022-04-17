3b875f94665d07a7947fede312224772d0a45eac
muralishc <muralish.clinton@circles.asia>
2022-04-17 18:37:49 +0530

    number of links

--- main.go
+++ main.go
@@ -7,11 +7,20 @@ import (
 )
 
 func main() {
+	link_count := 0
 	c := colly.NewCollector()
 
 	c.OnHTML("title", func(e *colly.HTMLElement) {
 		title := e.Text
-		fmt.Printf("Title: %s", title)
+		fmt.Printf("Title: %s\n", title)
 	})
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		// link := e.Attr("href")
+		link_count += 1
+	})
+
 	c.Visit("http://go-colly.org/")
+
+	fmt.Printf("Number of links: %d\n", link_count)
 }
